2cf15dfa1daf7983c4a1cb7aebecd01f1fc55bdf
RayhanRazu <rayhanrazusmu33@gmail.com>
2019-04-04 18:48:18 +0800

    modify 1 to 10 for loop

--- 1 to 10 for loop.cpp
+++ 1 to 10 for loop.cpp
@@ -5,9 +5,11 @@ int main ()
 {
 	int i;
 	cout << "the first natural numbers are:\n";
+	// forloop start
 	for (i=1;i<=10;i++)
 	{
-	cout <<i <<endl;
+		cout <<i <<endl;
     }
+    //forloop end
 	return 0;
 }
